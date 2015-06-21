8e993ca31d8e6d30f5922cc19e3e08382d053f94
Takafumi Kataoka <gyanexus7@gmail.com>
2015-06-21 16:53:47 +0900

    Initial Commit

---
+++ AppTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppTest2
+//
+//  Created by 片岡崇史 on 2015/06/21.
+//  Copyright (c) 2015年 gyane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
